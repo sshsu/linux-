4c7b5acaddf1b72a75b084e4ba2bd9e78cb8f53f
yishen <yishen.wang@foxmail.com>
2017-10-06 11:10:42 -0700

    使用select的监听socket的数据读取

--- 9_IO_Reuse/9-1_select_demo.cpp
+++ 9_IO_Reuse/9-1_select_demo.cpp
@@ -1,162 +1,130 @@
 /*
-此程序为epoll事件的EPOLLONESHOT事件示例
-EPOLLONESHOT实现通过为文件描述符设置标志位，该文件描述符监听的事件触发之后，标志位置假，然后该fd要再次触发事件的时候因为检查到标志为假说明正在对该文件进行操作，当前无法操作，
 
-*/
+ #include<sys/select.h>
+ int select(int nfds, fd_set* readfds, fd_set* writefds, fd_set* exceptfds, struct timeval* timeout)
+
+  函数说明:
+  此函数搜索[0, nfds）的文件描述符，检查是否有发生I/O事件(读，写，异常)，并将结果返回在readfds，writefds，writefds集合中
+
+  参数说明:
+  nfds:指定监听的文件描述符的区间为[0, nfds）
+  readfds:文件描述符发生读IO事件的记录集合
+  writefds:文件描述符发生写IO事件的记录集合
+  exceotfds:文件描述符发生异常事件的记录集合
+  timeout:select这个操作的等待时间
+
+  结构说明:
+  typedef struct{
+    int fds[];
+  }fd_set;
+  fd_set中有一个数组，数组的每个元素中的每个bit位代表对应的文件描述符
+
+  由于位操作太过于繁琐，可使用给下列宏对fd_set进行操作
+  #include<sys/select.h>
+  FD_ZERO (fd_set *fdset);              //清除fd_set所有位
+  FD_SET (int fd, fd_set *fdset);       //设置fd_set的位fd
+  FD_CLR (int fd, fd_set *fdset);       //清除fd_set的位fd
+  int FD_ISSET (int fd, fd_set *fdset); //测试fd_set的位fd是否被设置
+
+  一般的使用select的进行I/O监听复用的流程为
+  1.设置好监听集合readfds，writefds，exceotfds中的位，说明要位所对应文件描述符的IO事件(读，写，异常）
+  2.调用select，内核将对应监听的事件的结果返回在readfds，writefds，exceotfds中
+  3.使用FD_ISSET(fd, readfds)去判断文件是否有对应的事件触发
 
 
-#include<sys/types.h>
-#include<sys/socket.h>
-#include<netinet/in.h>
-#include<arpa/inet.h>
-#include<assert.h>
-#include<unistd.h>
-#include<errno.h>
-#include<string.h>
-#include<fcntl.h>
-#include<stdio.h>
-#include<sys/epoll.h>
-#include<pthread.h>
+
+  由于socket接收了带外数据之后，处于异常状态，使用select可以监测到这种状态，从而分别处理普通数据和带外数据，如下程序
+
+ */
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <assert.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <fcntl.h>
 #include <stdlib.h>
+#include <poll.h>
 
+#define BUF_SIZE 1024
 
-#define MAX_EVENT_NUMBER 1024
-#define BUFFER_SIZE 10
-
-int set_no_blocking(int fd ){
-    int old_option = fcntl( fd, F_GETFL );
-    int new_option = old_option | O_NONBLOCK;
-    fcntl( fd, F_SETFL, new_option );
-    return old_option;
-};
-
-void addfd( int epollfd, int fd, bool enable_et){
-    //创建事件对象，并初始化
-    epoll_event event;
-    event.data.fd = fd;
-    event.events = EPOLLIN;
-    //默认为lt模式，开启et模式
-    if( enable_et ){
-        event.events |= EPOLLET;
-    }
-    epoll_ctl( epollfd, EPOLL_CTL_ADD, fd, &event );
-    set_no_blocking( fd );
-}
-
-void lt( epoll_event* events, int number , int epollfd, int listenfd){
-    char buf[ BUFFER_SIZE ];
-    for( int i=0 ; i< number ; i++ ){
-        int sockfd = events[i].data.fd;
-        if( sockfd == listenfd){
-            struct sockaddr_in client_address;
-            socklen_t client_address_len = sizeof( client_address );
-            int connfd = accept(sockfd, (struct sockaddr*)&client_address, &client_address_len);
-            if( connfd < 0){
-                continue;
-            }
-            addfd( epollfd, connfd, false);
-        }
-        else if ( events[i].events & EPOLLIN){
-            printf("event trigger once\n");
-            memset( buf, 0, sizeof( buf ) );
-            int ret = recv( sockfd, buf, BUFFER_SIZE-1, 0);
-            if( ret <= 0 ){
-                close( sockfd );
-                continue;
-            }
-            printf( "get %d bytes of content: %s\n",ret, buf);
-        }
-        else{
-            printf("something else happened \n");
-        }
-
+int main(int argc, char* argv[])
+{
+    if( argc <= 2 )
+    {
+        printf("Usage: %s ip_address port_number\n", basename(argv[0]));
+        return 1;
     }
 
-}
+    const char* ip = argv[1];
+    int port = atoi(argv[2]);
 
-void et( epoll_event *events, int number, int epollfd, int listenfd){
-    char buf[ BUFFER_SIZE ];
-    for( int i = 0 ; i < number ; i++ ){
-        int sockfd = events[i].data.fd;
-        if( sockfd == listenfd ){
-            struct sockaddr_in client_address;
-            socklen_t client_address_len = sizeof( client_address );
-            int connfd = accept( sockfd, (struct sockaddr*)&client_address, &client_address_len );
-            if( connfd < 0){
-                continue;
+    int ret = 0;
+    struct sockaddr_in address;
+    bzero( &address, sizeof( address ));
+    address.sin_family = AF_INET;
+    inet_pton( AF_INET,  ip, &address.sin_addr );
+    address.sin_port = htons ( port );
+
+    int listenfd = socket( AF_INET, SOCK_STREAM, 0 );
+    assert( listenfd >= 0 );
+
+    ret = bind( listenfd, (struct sockaddr*)&address, sizeof(address));
+    assert( ret != -1 );
+
+    ret = listen( listenfd, 5 );
+    assert( ret != -1 );
+
+    struct sockaddr_in client_address;
+    socklen_t  client_address_len = sizeof( client_address );
+    int connfd = accept( listenfd, ( struct sockaddr* )&client_address, &client_address_len );
+    if( connfd < 0){
+        printf("\n");
+    }
+    else{
+
+        char buf[ BUF_SIZE ];//缓存socket发送过来的数据
+        fd_set readfds;      //创建写和异常的fd_set
+        fd_set exceptfds;
+        FD_ZERO( &readfds );  //清空fd_set
+        FD_ZERO( &exceptfds );
+
+        while( 1 ){
+            memset( buf, 0 , sizeof( buf ));
+            //设置要监听读和异常的文件描述符
+            FD_SET( connfd, &readfds );
+            FD_SET( connfd, &exceptfds );
+            //查看connfd是否发生读事件/异常，timeout为NULL表示select阻塞到事件发生
+            ret = select( connfd + 1, &readfds, NULL, &exceptfds, NULL );
+
+            if( ret < 0 ){
+                printf(" selection faliure\n");
+                break;
             }
-            addfd( epollfd, connfd, true);
-        }
-        else if( events[i].events & EPOLLIN ){
-            printf( "event trigger once\n ");
-            while(1){
-                memset( buf, 0, sizeof( buf ) );
-                int ret = recv ( sockfd, buf, BUFFER_SIZE -1 , 0 );
-                if( ret < 0   ){
-                    if(( errno == EAGAIN || errno == EWOULDBLOCK)){
-                        printf(" read later\n");
-                        break;
-                    }
-                    close( sockfd );
+
+            if( FD_ISSET( connfd, &readfds )){ //在返回的readfds检查到了connfd， 说明有读事件发生
+                ret = recv(connfd, buf, sizeof(buf) - 1, 0);
+                if( ret < 0 ){
                     break;
                 }
-                else if( ret == 0 ){
-                    close( sockfd );
-                }
-                else{
-                    printf("get %d bytes of content: %s\n", ret, buf );
-                }
+                printf("get %d bytes of normal data: %s\n", ret, buf);
 
             }
-
-        }
-        else {
-            printf( "something else happened \n");
+            else if( FD_ISSET( connfd, &exceptfds)){//在返回的readfds检查到了connfd， 说明有读带外事件发生
+                ret = recv(connfd, buf, sizeof(buf) - 1, 0);
+                if( ret < 0 ){
+                    break;
+                }
+                printf("get %d bytes of oob data: %s\n", ret, buf);
+            }
         }
 
     }
-}
-
-int main(int argc, char *argv[]){
-
-    if( argc < 3 ){
-        printf("usage : %s ip_number port number", basename( argv[0] ));
-        return -1;
-    }
-    const char *ip = argv[1];
-    int port = atoi( argv[2] );
-
-    struct sockaddr_in server_address;
-    bzero( &server_address, sizeof( server_address ));
-    server_address.sin_family = AF_INET;
-    inet_pton( AF_INET, ip, &server_address.sin_addr);
-    server_address.sin_port = htons( port );
-
-    int sockfd = socket(AF_INET, SOCK_STREAM, 0 );
-    assert( sockfd >= 0 );
-    int ret = bind (sockfd, (struct sockaddr*)&server_address, sizeof(server_address) );
-    assert( ret != -1 );
-    ret = listen( sockfd, 5);
-    assert( ret != -1 );
-
-    //创建事件对象数组
-    epoll_event events[ MAX_EVENT_NUMBER ];
-    //创建epoll对象
-    int epollfd = epoll_create( 5 );
-    assert( epollfd != -1 );
-    addfd( epollfd, sockfd, true );
-
-    while(1){
-        //获取触发的监听事件
-        ret = epoll_wait( epollfd, events, MAX_EVENT_NUMBER, -1 );
-
-        if( ret < 0 ){
-            printf( "epoll failure\n");
-        }
-
-//        lt( events, ret, epollfd, sockfd );
-        et( events, ret, epollfd, sockfd );
-
-    }
+    close( connfd );
+    close( listenfd );
     return 0;
 }
